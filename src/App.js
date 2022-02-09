83b06216672bf3d134c0f43ddd3eef7e29f24a74
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-02-09 12:38:06 -0800

    Finish with Contract

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ export default function App() {
   const [manager, setManager] = useState("");
   const [players, setPlayers] = useState([]);
   const [balance, setBalance] = useState("");
-
+  const [userEnterValue, setuserEnterValue] = useState("");
+  const [message, setMessage] = useState("");
   // web3.eth.getAccounts().then(console.log);
   useEffect(() => {
     const callManager = async () => {
@@ -19,7 +20,29 @@ export default function App() {
       setBalance(balance);
     };
     callManager();
-  }, []);
+  }, [userEnterValue]);
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    setMessage("You have submit the transaction, please wait.....");
+    const accounts = await web3.eth.getAccounts();
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(userEnterValue, "ether")
+    });
+
+    setMessage("Transaction success!");
+  }
+
+  async function onPick() {
+    const accounts = await web3.eth.getAccounts();
+    setMessage("waiting on the final transaction");
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+    setMessage("transaction success");
+  }
   return (
     <div className="App">
       {manager && (
@@ -32,6 +55,20 @@ export default function App() {
       {balance && (
         <h2>Current balance: {web3.utils.fromWei(balance, "ether")}</h2>
       )}
+      <hr />
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div>
+          <h4>Enter the ether you want to try : </h4>
+          <input
+            value={userEnterValue}
+            onChange={(e) => setuserEnterValue(e.target.value)}
+          />
+          <button>Enter</button>
+        </div>
+      </form>
+      <h2>{message}</h2>
+      <h2>Time to pick a winner </h2>
+      <button onClick={() => onPick()}>Pick a winner</button>
     </div>
   );
 }
