512bf9aff00e457588d8b17bc24647d178da5adb
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-02-09 11:36:58 -0800

    Embed the metamask web3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./styles.css";
+import web3 from "./web3";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  web3.eth.getAccounts().then(console.log);
+  return <div className="App"></div>;
 }
